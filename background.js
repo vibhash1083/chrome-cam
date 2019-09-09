faf552af8d0bf264dd2654e4df9abcefafb4d7b6
vibhash1083 <vibhash1083.07@bitmesra.ac.in>
2019-09-09 08:51:49 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,30 @@
+const vid = document.querySelector('#webcamVideo');
+
+// Setup webcam, initialize the KNN classifier model and start the work loop.
+async function setupCam() {
+  navigator.mediaDevices.getUserMedia({
+    video: true
+  }).then(mediaStream => {
+    vid.srcObject = mediaStream;
+  }).catch((error) => {
+    console.warn(error);
+  });
+  await knn.load();
+  setTimeout(loop, 50);
+}
+
+// If cam acecss has already been granted to this extension, setup webcam.
+chrome.storage.local.get('camAccess', items => {
+  if (!!items['camAccess']) {
+    console.log('cam access already exists');
+    setupCam();
+  }
+});
+
+// If cam acecss gets granted to this extension, setup webcam.
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if ('camAccess' in changes) {
+    console.log('cam access granted');
+    setupCam();
+  }
+});
---
+++ popup.js
@@ -0,0 +1,11 @@
+function setupCam() {
+  navigator.mediaDevices.getUserMedia({
+    video: true
+  }).then(mediaStream => {
+    document.querySelector('#webcamVideo').srcObject = mediaStream;
+  }).catch((error) => {
+    console.warn(error);
+  });
+}
+
+setupCam();
